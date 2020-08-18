273be8f2ffef0d11c671b74e3651a7e4df08657c
Stefan Schmidt <stefan.joachim.schmidt@gmail.com>
2020-08-18 15:18:05 +0200

    chapter 2 quiz answered.

--- 02_names_and_values.R
+++ 02_names_and_values.R
@@ -1,11 +1,12 @@
 ### Chapter 2 - Names and Values
 
 ## 2.1 Quiz
+
 # 1.
 df <- data.frame(runif(3), runif(3))
 names(df) <- c(1, 2)
 #
-df['3'] <- cbind(df[1] + df[2])
+df$`3` <- cbind(df$`1` + df$`2`)
 df
 colnames(df)
 rownames(df)
@@ -13,11 +14,18 @@ rownames(df)
 # 2.
 x <- runif(1e6)
 y <- list(x, x, x)
+#
+sx <- object.size(x)
+print(sx, units="auto", standard = "SI")
+sy <- object.size(y)
+print(sy, units="auto", standard = "SI")
 
 # 3.
 a <- c(1, 5, 3, 2)
 b <- a
-b[[1]]
+b[[1]] <- 10
+#
+# line 3 because lazy?
 
 
 ## 2.2 Binding Basics
